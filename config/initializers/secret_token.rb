36e22a4f9de3b289378a0b542e0c3f814a043ed9
Kweku kankam <kweku360@gmail.com>
2014-02-20 20:23:20 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roadalert::Application.config.secret_token = '186d56118ccfdc701f729864c0a164930105fc7bea13de74c82d6b0ffce3cae5a1ca7bb0db8de8b1a8456c2a2c840f9b78d4eca5d833c4707f03472faa877ba2'
